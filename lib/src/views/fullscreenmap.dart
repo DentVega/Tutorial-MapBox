8a318edcf51e0bc6c2da28b0be1043744f41c196
Brian Vega <dentvega6@gmail.com>
2020-09-05 19:58:50 -0400

    - add mapBox

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-class FullScreenMap extends StatelessWidget {
+class FullScreenMap extends StatefulWidget {
+  @override
+  _FullScreenMapState createState() => _FullScreenMapState();
+}
+
+class _FullScreenMapState extends State<FullScreenMap> {
+  MapboxMapController mapboxMapController;
+
+  void _onMapCreated(MapboxMapController controller) {
+    mapboxMapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hola Mundo"),
-      ),
+      body: MapboxMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+              zoom: 15, target: LatLng(37.810575, -122.477174))),
     );
   }
 }
