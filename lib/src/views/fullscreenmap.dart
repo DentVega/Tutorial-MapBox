30469a5a1b877214f92540af2034eee798b7996e
Brian Vega <dentvega6@gmail.com>
2020-09-05 20:57:22 -0400

    - Cambiar el estilo de nuestro mapa

--- lib/src/views/fullscreenmap.dart
+++ lib/src/views/fullscreenmap.dart
@@ -8,6 +8,10 @@ class FullScreenMap extends StatefulWidget {
 
 class _FullScreenMapState extends State<FullScreenMap> {
   MapboxMapController mapboxMapController;
+  final center = LatLng(37.810575, -122.477174);
+  final dark = 'mapbox://styles/brianvh/ckeqck6oq1cvo19mtjmv6ff9q';
+  final streetsStyle = 'mapbox://styles/brianvh/ckeqcmlr81f2m19qs0us09ivv';
+  String selectedStyle = 'mapbox://styles/brianvh/ckeqck6oq1cvo19mtjmv6ff9q';
 
   void _onMapCreated(MapboxMapController controller) {
     mapboxMapController = controller;
@@ -16,10 +20,31 @@ class _FullScreenMapState extends State<FullScreenMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MapboxMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-              zoom: 15, target: LatLng(37.810575, -122.477174))),
+      body: createMap(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                if (selectedStyle == dark) {
+                  selectedStyle = streetsStyle;
+                } else {
+                  selectedStyle = dark;
+                }
+              });
+            },
+            child: Icon(Icons.add_to_home_screen),
+          )
+        ],
+      ),
     );
+  }
+
+  MapboxMap createMap() {
+    return MapboxMap(
+        styleString: selectedStyle,
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: center, zoom: 14));
   }
 }
